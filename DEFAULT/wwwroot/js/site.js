d54a335a728ab7da8df96101c97715a4d037796c
HAFSA TATA <hafsatata.work@gmail.com>
2023-12-17 00:28:37 +0100

    first commit : HEADER started

---
+++ DEFAULT/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script>
+    $(document).ready(function () {
+        $('.icon-click').on('click', function () {
+            $(this).toggleClass('clicked');
+        });
+    });
+</script>
+
+
