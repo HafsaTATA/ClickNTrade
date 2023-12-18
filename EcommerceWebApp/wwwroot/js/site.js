939411207ddc38ce0e79b043c6ae5de883066859
HAFSA TATA <hafsatata.work@gmail.com>
2023-12-18 15:52:56 +0100

    STATE 1

--- EcommerceWebApp/wwwroot/js/site.js
+++ EcommerceWebApp/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('.icon-click').on('click', function () {
+        $(this).toggleClass('clicked');
+    });
+});
+
+$("#vehicules").on("click", function () {
+    window.location.href = "/vehicule";
+});
+
+$("#electronics").on("click", function () {
+    window.location.href = "/electronics";
+});
+
+$("#clothes").on("click", function () {
+    window.location.href = "/clothes";
+});
+
+
