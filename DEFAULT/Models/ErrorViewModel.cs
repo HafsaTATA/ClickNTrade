db5709ad17a203b148459a3e7afda9f2e3f3856c
HAFSA TATA <hafsatata.work@gmail.com>
2023-12-18 15:39:38 +0100

    BACK TO 0

--- DEFAULT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DEFAULT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
